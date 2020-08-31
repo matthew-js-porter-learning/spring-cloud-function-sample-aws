e5782a7f8d75b776781b8c28c697db94d3436c25
Matt Porter <matthew.js.porter@gmail.com>
2020-08-30 21:26:50 -0400

    adding working example that was deployed to aws

---
+++ src/main/java/example/MyStringHandlers.java
@@ -0,0 +1,6 @@
+package example;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class MyStringHandlers extends SpringBootRequestHandler<String, String> {
+}
